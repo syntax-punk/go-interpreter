70475b387c653aac6b60c590dac324bd2933e77b
syntax-punk <david.jaeren@gmail.com>
2024-06-07 21:18:24 +0200

    Added digit reading and saving token type INT

--- monkey/lexer/lexer.go
+++ monkey/lexer/lexer.go
@@ -55,6 +55,10 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
@@ -73,6 +77,16 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[position:l.position]
 }
 
+func (l *Lexer) readNumber() string {
+	position := l.position
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
@@ -85,4 +99,8 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -9,11 +9,10 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 	let ten = 10;
-
-	let add = fn(x, y) {
-		x + y;
+		let add = fn(x, y) {
+			x + y;
 	};
-
+	
 	let result = add(five, ten);
 	`
 
